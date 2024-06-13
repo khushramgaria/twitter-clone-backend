6b48ae1a2de0b2f38891dbf49118d319846f8a3d
Khushpreet Singh <khushramgaria@gmail.com>
2024-06-13 15:38:58 +0530

    cookie-parser cors package install and use. also make utilities of APIRESPONSE & APIERROR

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//to set limit to take json
+app.use(express.json({
+    limit: "16kb"
+}))
+
+//to understand the url data 
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+//
+app.use(express.static("public"))
+
+//to perform CRUD operations of user cookies browser with server securly 
+app.use(cookieParser())
+
+export { app }
